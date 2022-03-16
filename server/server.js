c5cfa570a8daf803fee9372eece105c986d62f9d
uheaji <yoolbk@naver.com>
2022-03-16 15:11:16 +0900

    ◽ 외부에서 파일접근, 이미지 업로드 필터 걸어주기

--- server/server.js
+++ server/server.js
@@ -7,10 +7,20 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "./uploads"), // 파일 저장장소
   filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`), // 파일명
 });
-const upload = multer({storage}); // upload는 미들웨어
+const upload = multer({
+    storage, fileFilter: (req, file, cb) => { // 이미지 필터
+        if (["image/jpeg", "image/png"].includes(file.mimetype)) cb(null, true)
+        else cb(new Error("invalid file type"), false);
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 5 // 5메가바이트
+    }
+}); // upload는 미들웨어
 
 const app = express();
 const PORT = 5000;
+
+app.use(express.static("uploads")); // 외부에서 파일접근
 
 app.post('/upload', upload.single("imageTest"), (req, res) => {
     console.log(req.file);
